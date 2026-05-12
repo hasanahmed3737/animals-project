f95796d809041ddb8d2ff664b842a379bdf1e843
danielelhawe-crypto <danielelhawe@gmail.com>
2026-05-12 06:18:16 +0300

    Final authorized version by [danial] Subsequent changes by others are not under my supervision

--- js/shop.js
+++ js/shop.js
@@ -5,17 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
     searchInput.addEventListener("input", () => {
       const query = searchInput.value.toLowerCase().trim();
       
-      // نختار كل حاويات الكروت في الصفحة
+
       document.querySelectorAll(".cards-container").forEach(container => {
-        // نأخذ النصوص اللي عايزين نبحث جواها (الاسم، المكان، التاجات)
+   
         const name = container.querySelector("h3")?.textContent.toLowerCase() || "";
         const location = container.querySelector(".card-location")?.textContent.toLowerCase() || "";
         const tags = [...container.querySelectorAll(".tag")].map(t => t.textContent.toLowerCase()).join(" ");
 
-        // نتحقق إذا كانت كلمة البحث موجودة في أي منهم
+      
         const matches = name.includes(query) || location.includes(query) || tags.includes(query);
 
-        // إذا وجدنا تطابق نظهر الكارت، وإذا لم نجد نخفيه
+    
         container.style.display = matches ? "" : "none";
       });
     });
